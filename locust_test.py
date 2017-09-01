94882bae6fdde6730ee79856edfe94f27f7b2b49
evancircle <3101893277@qq.com>
2017-09-01 17:21:49 +0800

    fitst commit file

---
+++ locust_test.py
@@ -0,0 +1,20 @@
+from locust import HttpLocust,TaskSet,task
+import json
+
+def postdata(self):
+    self.client.post("/dlo/report_data",{"no":"BLtotest","type":"getuuid"})
+
+
+class UserBehavior(TaskSet):
+    tasks = {postdata:1}
+
+    def on_start(self):
+        postdata(self)
+
+class WebUserLocust(HttpLocust):
+    task_set = UserBehavior
+    min_wait = 5000
+    max_wait = 10000
+
+#if __name__ == '__main__':
+#    subprocess.Popen('locust -f .\locust_test.py --host=http://api.epoque.cn', shell=True)
